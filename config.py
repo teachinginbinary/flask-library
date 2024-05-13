7e2ed0c59d12ad4ddf05f0e177e96706b3bb029d
teachinginbinary <teachinginbinary@gmail.com>
2024-05-13 01:00:00 -0400

    six commit

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import secrets
 
 SECRET_KEY = secrets.token_hex(16)
-SQLALCHEMY_DATABASE_URI = 'postgres://teachinginbinary:3OGuBhuYBPGkd1Xkv6nd23iqe73k91vT@dpg-cp0pgm021fec7388mkn0-a.oregon-postgres.render.com/library_db_pl7g'
+SQLALCHEMY_DATABASE_URI = 'postgresql://teachinginbinary:3OGuBhuYBPGkd1Xkv6nd23iqe73k91vT@dpg-cp0pgm021fec7388mkn0-a.oregon-postgres.render.com/library_db_pl7g'
